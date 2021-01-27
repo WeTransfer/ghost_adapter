d405f179189cb5f2e69e14c16bb637e5701caa13
Austin C Roos <austin.roos@wetransfer.com>
2021-01-27 22:51:03 +0100

    Run rubocop to clean up some formatting

--- ghost_adapter.gemspec
+++ ghost_adapter.gemspec
@@ -12,7 +12,6 @@ Gem::Specification.new do |spec|
   spec.license       = "MIT"
   spec.required_ruby_version = Gem::Requirement.new(">= 2.3.0")
 
-
   spec.metadata["homepage_uri"] = spec.homepage
   spec.metadata["source_code_uri"] = spec.homepage
 
--- lib/active_record/connection_adapters/ghost_mysql2_adapter.rb
+++ lib/active_record/connection_adapters/ghost_mysql2_adapter.rb
@@ -43,7 +43,7 @@ module ActiveRecord
 
       def execute(sql, name = nil)
         # Only ALTER TABLE statements are automatically skipped by gh-ost
-        # We need to manually skip CREATE TABLE, DROP TABLE, and 
+        # We need to manually skip CREATE TABLE, DROP TABLE, and
         # INSERT/DELETE (to schema migrations) for dry runs
         return if dry_run && should_skip_for_dry_run?(sql)
 
@@ -57,47 +57,48 @@ module ActiveRecord
       private
 
       attr_reader :dry_run
-  
+
       ALTER_TABLE_REGEX = /\AALTER\s+TABLE\W*(?<table_name>\w+)\W*(?<query>.*)$/i
-  
+
       def parse_sql(sql)
         capture = sql.match(ALTER_TABLE_REGEX)
         return if capture.nil?
+
         captured_names = capture.names
         return unless captured_names.include? 'table_name'
         return unless captured_names.include? 'query'
-  
-        [ capture[:table_name], clean_query(capture[:query]) ]
+
+        [capture[:table_name], clean_query(capture[:query])]
       end
-  
+
       def clean_query(query)
         cleaned = query.gsub(/[^0-9a-z_\s\(\)\:\'\"\{\}]/i, '')
         cleaned.gsub('"', '\"')
       end
-  
+
       def should_skip_for_dry_run?(sql)
         if create_or_drop_table?(sql)
           puts 'Skipping CREATE TABLE or DROP TABLE for dry run'
           puts 'SQL:'
           puts sql
         end
-  
+
         create_or_drop_table?(sql) || schema_migration_update?(sql)
       end
-  
+
       def create_or_drop_table?(sql)
         /\Acreate\stable/i =~ sql ||
           /\Adrop\stable/i =~ sql
       end
-  
+
       def schema_migration_update?(sql)
         /\Ainsert\sinto\s`schema_migrations`/i =~ sql ||
           /\Adelete\sfrom\s`schema_migrations`/i =~ sql
       end
-  
+
       def build_ghost_command(table, query)
         replica_server_id = rand(100000)
-  
+
         command = [
           'gh-ost',
           '--max-load=Threads_running=150',
@@ -124,28 +125,28 @@ module ActiveRecord
           "--serve-socket-file=/tmp/ghost.#{migration_id(table)}.sock",
           "--replica-server-id=#{replica_server_id}",
         ]
-  
+
         unless migration_db_password.blank?
           command << "--password=#{migration_db_password}"
         end
-  
+
         if Rails.env.development?
           command << '--allow-on-master'
         end
-  
+
         unless dry_run
           command << '--initially-drop-ghost-table'
           command << '--initially-drop-old-table'
           command << '--ok-to-drop-table'
           command << '--execute'
         end
-  
+
         command
       end
-  
+
       def run_ghost(table, query)
         ghost_command = build_ghost_command(table, query)
-  
+
         Open3.popen2e(*ghost_command) do |_stdin, stdout_stderr, wait_thread|
           stdout_stderr.each_line do |line|
             if ready_to_cutover?(line)
@@ -155,17 +156,18 @@ module ActiveRecord
             end
             puts "[gh-ost]:\t#{line}"
           end
-  
+
           unless wait_thread.value.success?
             raise GhostExecutionError.new("gh-ost migration failed. exit code: #{wait_thread.value.exitstatus}")
           end
         end
-  
+
         cooldown
       end
-  
+
       def cooldown
         return if dry_run
+
         puts 'Cooling down for 5 seconds...'
         5.times do
           sleep 1
@@ -173,41 +175,41 @@ module ActiveRecord
         end
         puts ''
       end
-  
+
       def ready_to_cutover?(output_line)
         match = /\sState:\s(?<state>[^;]*)/.match(output_line)
         return false if match.nil?
-  
+
         state = match[:state] rescue nil
         return false if state.nil?
-  
+
         state.downcase == 'postponing cut-over'
       end
-  
+
       def migration_id(table)
         @migration_id ||= "#{table}_#{SecureRandom.hex}"
       end
-  
+
       def cutover_flag_file(table)
         "/tmp/ghost.postpone.#{migration_id(table)}.flag"
       end
-  
+
       def db_name
         ENV['DATABASE_NAME']
       end
-  
+
       def main_db_host
         ENV['DATABASE_MAIN_HOST']
       end
-  
+
       def read_replica_db_url
         ENV['DATABASE_READ_REPLICA_HOST']
       end
-  
+
       def migration_db_user
         ENV['DATABASE_MIGRATION_USER']
       end
-  
+
       def migration_db_password
         ENV['DATABASE_MIGRATION_PASSWORD']
       end
