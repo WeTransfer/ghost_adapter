7a30c77bc0e34840016908f821210f4d589438e1
Austin C Roos <austin.roos@wetransfer.com>
2021-03-18 21:13:14 +0100

    Fix specs

--- spec/ghost_adapter/env_parser_spec.rb
+++ spec/ghost_adapter/env_parser_spec.rb
@@ -9,73 +9,79 @@ RSpec.describe GhostAdapter::EnvParser do
 
   context 'string values' do
     it 'reads string values and does no conversion' do
-      env = { 'GHOST_KEY' => 'test' }
+      env = { 'GHOST_DEBUG' => 'test' }
       config = described_class.new(env).config
-      expect(config['key']).to eq 'test'
+      expect(config[:debug]).to eq 'test'
     end
   end
 
   context 'converting number values' do
     it 'converts integer values' do
-      env = { 'GHOST_KEY' => '123' }
+      env = { 'GHOST_DEBUG' => '123' }
       config = described_class.new(env).config
-      expect(config['key']).to eq 123
+      expect(config[:debug]).to eq 123
     end
 
     it 'converts float values' do
-      env = { 'GHOST_KEY' => '1.23' }
+      env = { 'GHOST_DEBUG' => '1.23' }
       config = described_class.new(env).config
-      expect(config['key']).to eq 1.23
+      expect(config[:debug]).to eq 1.23
     end
   end
 
   context 'converting boolean values' do
     it 'converts "y" to true' do
-      env = { 'GHOST_KEY' => 'y' }
+      env = { 'GHOST_DEBUG' => 'y' }
       config = described_class.new(env).config
-      expect(config['key']).to be true
+      expect(config[:debug]).to be true
     end
 
     it 'converts "yes" to true' do
-      env = { 'GHOST_KEY' => 'yes' }
+      env = { 'GHOST_DEBUG' => 'yes' }
       config = described_class.new(env).config
-      expect(config['key']).to be true
+      expect(config[:debug]).to be true
     end
 
     it 'converts "t" to true' do
-      env = { 'GHOST_KEY' => 't' }
+      env = { 'GHOST_DEBUG' => 't' }
       config = described_class.new(env).config
-      expect(config['key']).to be true
+      expect(config[:debug]).to be true
     end
 
     it 'converts "true" to true' do
-      env = { 'GHOST_KEY' => 'true' }
+      env = { 'GHOST_DEBUG' => 'true' }
       config = described_class.new(env).config
-      expect(config['key']).to be true
+      expect(config[:debug]).to be true
     end
 
     it 'converts "n" to false' do
-      env = { 'GHOST_KEY' => 'n' }
+      env = { 'GHOST_DEBUG' => 'n' }
       config = described_class.new(env).config
-      expect(config['key']).to be false
+      expect(config[:debug]).to be false
     end
 
     it 'converts "no" to false' do
-      env = { 'GHOST_KEY' => 'no' }
+      env = { 'GHOST_DEBUG' => 'no' }
       config = described_class.new(env).config
-      expect(config['key']).to be false
+      expect(config[:debug]).to be false
     end
 
     it 'converts "f" to false' do
-      env = { 'GHOST_KEY' => 'f' }
+      env = { 'GHOST_DEBUG' => 'f' }
       config = described_class.new(env).config
-      expect(config['key']).to be false
+      expect(config[:debug]).to be false
     end
 
     it 'converts "false" to false' do
-      env = { 'GHOST_KEY' => 'false' }
+      env = { 'GHOST_DEBUG' => 'false' }
       config = described_class.new(env).config
-      expect(config['key']).to be false
+      expect(config[:debug]).to be false
+    end
+
+    it 'is case insensitive' do
+      env = { 'GHOST_DEBUG' => 'NO' }
+      config = described_class.new(env).config
+      expect(config[:debug]).to be false
     end
   end
 end
